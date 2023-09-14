e038b28c3febf4198917639c5de411a737a76497
ltp <ltp@localhost.localdomain>
2023-09-13 21:18:52 -0400

    celluloid kps

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update system
+echo 'Updating system...'
+sudo apt update
+
+# Install Celluloid
+echo 'Installing Celluloid...'
+sudo apt install celluloid -y
+
+
+# Install KeepassXC 
+echo 'Installing KepassXC...'
+sudo apt install keepassxc -y
